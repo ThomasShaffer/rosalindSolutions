8107f17050150033a6e693457fb32883adee2cb7
Your Name <you@example.com>
2021-04-17 20:49:04 -0700

    Implement specialized fibonacci algorithm using memoization (recursion)

---
+++ population_fibonacci/memoized_fib.py
@@ -0,0 +1,30 @@
+#!usr/bin/env python
+import sys
+
+def args() -> [int,int]:
+	try:
+		return [int(sys.argv[1]), int(sys.argv[2])]
+	except:
+		return 'Incorrect input format. Please input two integers.'
+
+def rabbit_fib(generation, litter, memory):
+	if generation in memory:
+		return memory[generation]
+	if generation <= 2:
+		return 1
+	memory[generation] = rabbit_fib(generation-1,litter,memory) + (litter* rabbit_fib(generation-2,litter,memory))
+	return memory[generation]
+										
+
+def main() -> int:
+	arguments = args()
+	generations = arguments[0]
+	litter_size = arguments[1]
+	memo = {}
+	size = rabbit_fib(generations, litter_size, memo)
+	print(size)
+
+if __name__ == '__main__':
+	main()
+
+
