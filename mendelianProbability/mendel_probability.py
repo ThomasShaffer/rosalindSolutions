26af97db7459df36d79791a85df3957bf730b788
Your Name <you@example.com>
2021-04-28 01:21:26 -0700

    fix command line parser

--- mendelianProbability/mendel_probability.py
+++ mendelianProbability/mendel_probability.py
@@ -4,7 +4,7 @@ import sys
 
 def args():
 	try:
-		return [i for sys.argv[i] in range(1,4)]
+		return [int(sys.argv[i]) for i in range(1,4)]
 	except:
 		return 'Incorrect input format. Please input 3 separate integers'
 
@@ -13,16 +13,17 @@ def dominant_probability(numbers: list) -> float:
 	#AA -> homozygous dominant
 	#Aa -> heterozygous
 	#aa -> homozyogus recessive
-
 	AA = numbers[0]
 	Aa = numbers[1]
 	aa = numbers[2]
 
+	total_permutations = AA * Aa * aa
+	recessive_only = (Aa * aa) * (1/2)
+	return 1 - (recessive_only / total_permutations)
 
 def main() -> None:
 	population = args()
-	probability = dominant_probability(args)
+	probability = dominant_probability(population)
 	print(probability)
-
 if __name__ == '__main__':
 	main()
